3a1b566b23f67e74396d0b5bae845a8b2632a639
Luís Felipe de Abreu Marciano <126877678+lfamarciano@users.noreply.github.com>
2025-04-06 23:26:05 -0300

    Documentando os métodos do df

--- framework/dataframe.py
+++ framework/dataframe.py
@@ -1,25 +1,68 @@
 class DataFrame:
-    def __init__(self, columns, rows):
+    def __init__(self, columns : list[str], rows : list[list]:
         self.columns = columns # lista de str com o nome das colunas
         self.rows = rows  # lista de listas ou lista de dicionários
 
     def to_dicts(self):
+        """
+        Retorna uma versão do DataFrame como um dicionário.
+
+        Saída:
+            dict: dicionário onde as chaves são os nomes das colunas
+            e os valores são as linhas do DataFrame.
+        """
         return [dict(zip(self.columns, row)) for row in self.rows]
 
-    def add_row(self, row):
+    def add_row(self, row : list):
+        """
+        Adiciona a linha ao DataFrame.
+
+        Entrada:
+            row (list): uma lista de formato equivalente às linhas
+            do DataFrame.
+        """
         if len(row) != len(self.columns):
             raise ValueError("Número de colunas incorreto.")
         self.rows.append(row)
 
-    def vstack(self, df):
+    def vstack(self, df : DataFrame):
+        """
+        Dado outro DataFrame com o mesmo número de colunas, empilha
+        ele abaixo do DataFrame em que o método está sendo aplicado.
+
+        Entrada:
+            df(DataFrame): DataFrame a ser empilhado abaixo.
+        """
         if len(df.columns) != len(self.columns):
             raise ValueError("Número de colunas incorreto.")
         self.rows.extend(df.rows)
     
     def shape(self):
+        """
+        Retorna uma tupla com o número de linhas e colunas do DataFrame
+
+        Saída:
+            Tuple(int,int): (Número de linhas, Número de colunas)
+        """
         return (len(self.rows), len(self.columns))
 
-    def group_by(self, column_name, agg_fn, agg_columns=None):
+    def group_by(self, column_name : str, agg_fn : function, agg_columns=None : list[str]):
+        """
+        Retorna uma versão agrupada do DataFrame pela coluna column_name,
+        aplicando a função agg_fn nas colunas indicadas em agg_columns e
+        descartando as que não foram indicadas. Se agg_columns = None 
+        agg_fn é aplicada em todas as colunas.
+
+        Entrada:
+            column_name(str): Nome da coluna que servirá de referencia
+            para o agrupamento.
+            agg_fn: Função que sumarizará as colunas não agrupadas
+            indicadas em agg_columns.
+            agg_columns(list[str]): Lista das colunas que são sumarizadas.
+
+        Saída:
+            DataFrame: DataFrame após o agrupamento feito.
+        """
         col_idx = self.columns.index(column_name)
         grouped = {}
 
@@ -43,7 +86,21 @@ class DataFrame:
         new_columns = [column_name] + [f"{agg_fn.__name__}({col})" for col in agg_columns]
         return DataFrame(new_columns, new_rows)
 
-    def sort_by(self, column_name, ascending=True):
+    def sort_by(self, column_name : str, ascending=True : bool):
+        """
+        Ordena as linhas do DataFrame com relação a coluna column_name.
+        Se ascending = True a ordenação é crescente se = False é 
+        decrescente.
+
+        Entrada:
+            column_name(str): Nome da coluna de referência para ordenação
+            das linha do DataFrame.
+            ascending(bool): True para ordenação crescente e False para
+            descrescente.
+
+        Saída:
+            DataFrame: DataFrame com as linhas ordenadas.
+        """
         col_idx = self.columns.index(column_name)
         sorted_rows = sorted(self.rows, key=lambda row: float(row[col_idx]), reverse=not ascending)
         return DataFrame(self.columns, sorted_rows)
