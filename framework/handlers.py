0289339b8d2b5ccaf620a2079d81e4a3a64ebbd4
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2025-04-21 12:42:10 -0300

    Handlers de métricas

--- framework/handlers.py
+++ framework/handlers.py
@@ -14,7 +14,7 @@ def process_new_orders():
     for order in new_orders.rows:
         id = orders_history.rows[-1][0] + 1
         cliente_id = order[0]
-        data_pedido = dt.datetime.now()
+        data_pedido = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         status = "novo"
         centro_logistico_id = order[3]
         valor_total = round(random.uniform(50, 5000), 2)
@@ -37,10 +37,40 @@ def update_stock():
 
     return produtos
 
+def top5_mais_vendidos(orders_history):
+    pass
+
+def valor_total_vendas(orders_history):
+    total = 0
+    for i in orders_history.rows:
+        total += i[5]
+    return total
+
+def vendas_por_cat_produto(orders_history):
+    result = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0}
+    for i in orders_history.rows:
+        result[i[4]] += 1
+
+    return dict(sorted(result.items(), key=lambda x: x[1]))
+
+def centros_mais_requisitados(orders_history):
+    result = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0}
+    for i in orders_history.rows:
+        result[i[4]] += 1
+
+    return dict(sorted(result.items(), key=lambda x: x[1]))
+
+def alerta_reposição(produtos):
+    produtos_em_alerta = []
+    for i in produtos.rows:
+        if i[4] <= 10:
+            produtos_em_alerta.append(i[1])
+    return produtos_em_alerta
 
 if __name__ == "__main__":
     orders_history = process_new_orders()
     print(orders_history)
-    produtos = update_stock()
-    print(produtos)
+    print(centros_mais_requisitados(orders_history))
+    print(valor_total_vendas(orders_history))
+    print(alerta_reposição(update_stock()))
 
